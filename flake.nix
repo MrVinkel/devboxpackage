67ae41e0de609f2666753ed41abc5c4fec6ffd51
Martin Vinkel <mrvinkel@gmail.com>
2025-01-11 16:53:51 +0100

    add flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,56 @@
 {
-	buildGoModule,
-	fetchFromGitLab,
-	lib,
-  }:
-  
-  buildGoModule rec {
-	pname = "devboxpackage";
-	version = "0.0.1";
-  
-	src = fetchFromGitLab {
-	  owner = "mr_vinkel";
-	  repo = pname;
-	  rev = "v${version}";
-	  hash = "sha256-d638eaf366524e3017ee8bfabded5a7ed81a59de";
-	};
-  
-	vendorHash = null;
-  
-	meta = with lib; {
-	  description = "Test package";
-	  homepage = "https://gitlab.com/mr_vinkel/devboxpackage";
-	  changelog = "https://gitlab.com/mr_vinkel/devboxpackage/-/releases";
-	  license = licenses.unlicense;
-	  maintainers = with maintainers; [ mr_vinkel ];
-	};
-  }
+  description = "devboxpackage test";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        lastTag = "v0.0.1";
+
+        revision =
+          if (self ? shortRev)
+          then "${self.shortRev}"
+          else "${self.dirtyShortRev or "dirty"}";
+
+        # Add the commit to the version string for flake builds
+        version = "${lastTag}";
+
+        vendorHash = null;
+
+        buildGoModule = pkgs.buildGo123Module;
+
+      in
+      {
+        inherit self;
+        packages.default = buildGoModule {
+          pname = "devboxpackage";
+          inherit version vendorHash;
+
+          src = ./.;
+
+          ldflags = [
+            "-s"
+            "-w"
+          ];
+
+          # Disable tests if they require network access or are integration tests
+          doCheck = false;
+
+          nativeBuildInputs = [ pkgs.installShellFiles ];
+
+          meta = with pkgs.lib; {
+            description = "devbox package test"; 
+            homepage = "https://gitlab.com/mr_vinkel/devboxpackage";
+            license = licenses.unlicense;
+            maintainers = with maintainers; [ mr_vinkel ];
+          };
+        };
+      }
+    );
+}
